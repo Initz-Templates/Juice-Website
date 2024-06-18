23f30f2afc70c4f7071d3de4bf18589ea339d966
Utkarsh Katiyar <UKatiyar@initializ.io>
2024-06-18 12:05:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
+import Header from './components/Header';
+import JuiceList from './components/JuiceList';
+import AboutUs from './components/AboutUs';
+import FeaturedProducts from './components/FeaturedProducts';
+import ContactUs from './components/ContactUs';
+import Reviews from './components/Reviews';
+import HeroCarousel from './components/Hero'; // Make sure this file is correctly named
+import JuiceDetail from './components/JuiceDetail';
+import Footer from './components/Footer';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <CssBaseline />
+      <Header />
+      <HeroCarousel />
+      <Routes>
+        <Route exact path="/" element={<JuiceList />} /> {/* Wrap JuiceList in Route component */}
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/featured-products" element={<FeaturedProducts />} />
+        <Route path="/contact" element={<ContactUs />} />
+        <Route path="/juice/:id" element={<JuiceDetail />} /> {/* Route for JuiceDetail */}
+      </Routes>
+      <Reviews />
+      <Footer/>
+    </Router>
   );
-}
+};
 
 export default App;
